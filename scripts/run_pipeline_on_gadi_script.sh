db0fbcf92281543ab0ac15ed4583d8662db923e2
georgiesamaha <georgina.samaha@sydney.edu.au>
2023-12-21 17:21:00 +1100

    Clean variables from gadi script

--- scripts/run_pipeline_on_gadi_script.sh
+++ scripts/run_pipeline_on_gadi_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
   
-#PBS -P [project code]
+#PBS -P 
 #PBS -N Somatic-shortV
-#PBS -l walltime=02:00:00
+#PBS -l walltime=10:00:00
 #PBS -l ncpus=1
 #PBS -l mem=20GB
 #PBS -W umask=022
@@ -10,27 +10,22 @@
 #PBS -e Somatic-shortV-nf.e
 #PBS -o Somatic-shortV-nf.o
 #PBS -l wd
-#PBS -l storage=scratch/[project code]+gdata/[project code]
+#PBS -l storage=
 #PBS -l jobfs=10GB
 
 # Load singularity and nextflow modules
 module load nextflow/22.04.3
 module load singularity
 
-
 export NXF_SINGULARITY_CACHEDIR=/scratch/$PROJECT/$(whoami)/singularity
 
 # Fill in these variables for your run
-
-samples=[FULL_PATH]/samples.csv
-ponvcf=[FULL_PATH]/pon.vcf.gz
-ref=[FULL_PATH]/hs38DH.fasta
-small_exac_common=[FULL_PATH]/small_exac_common_3.hg38.vcf.gz
-intervalList_path=[FULL_PATH]/Intervals_13/
-
+samples=
+ponvcf=
+ref=
+small_exac_common=
+intervalList_path=
 outDir=results
-
-
 
 # Run the pipeline 
 nextflow run main.nf -resume \
